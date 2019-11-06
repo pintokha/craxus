91c3c8cef998c1d9dc1e5ec7a5b9ed7ed1887e43
pintokha17 <pintokha17@gmail.com>
2019-11-06 18:21:00 +0100

    first working version

---
+++ src/Watcher.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Pintokha\Craxus;
+
+use GuzzleHttp\Client;
+use PHPUnit\Runner\BeforeTestHook;
+use ReflectionClass;
+use PHPUnit\Runner\AfterIncompleteTestHook;
+use PHPUnit\Runner\AfterLastTestHook;
+use PHPUnit\Runner\AfterRiskyTestHook;
+use PHPUnit\Runner\AfterSkippedTestHook;
+use PHPUnit\Runner\AfterSuccessfulTestHook;
+use PHPUnit\Runner\AfterTestErrorHook;
+use PHPUnit\Runner\AfterTestFailureHook;
+
+final class Watcher implements BeforeTestHook, AfterLastTestHook,
+    AfterSuccessfulTestHook, AfterTestFailureHook, AfterTestErrorHook,
+    AfterRiskyTestHook, AfterSkippedTestHook, AfterIncompleteTestHook
+{
+    protected $results = [];
+    protected $exceptions = ['Warning'];
+
+    protected $class;
+    protected $method;
+    protected $testID;
+    protected $docComment;
+
+    protected const TEST_SUCCESS        = '.';
+    protected const TEST_FAILURE        = 'F';
+    protected const TEST_ERROR          = 'E';
+    protected const TEST_RISKY          = 'R';
+    protected const TEST_SKIPPED        = 'S';
+    protected const TEST_INCOMPLETE     = 'I';
+
+    public function executeBeforeTest(string $test): void
+    {
+        if(!in_array($test, $this->exceptions)) {
+            try { $this->setTestData($test); }
+            catch (\Exception $exception) { var_dump($exception->getMessage()); }
+        }
+    }
+
+    public function executeAfterSuccessfulTest(string $test, float $time): void
+    {
+        $this->setResult(self::TEST_SUCCESS, $test, '', $time);
+    }
+
+    public function executeAfterTestFailure(string $test, string $message, float $time): void
+    {
+        $this->setResult(self::TEST_FAILURE, $test, $message, $time);
+    }
+
+    public function executeAfterTestError(string $test, string $message, float $time): void
+    {
+        $this->setResult(self::TEST_ERROR, $test, $message, $time);
+    }
+
+    public function executeAfterRiskyTest(string $test, string $message, float $time): void
+    {
+        $this->setResult(self::TEST_RISKY, $test, $message, $time);
+    }
+
+    public function executeAfterSkippedTest(string $test, string $message, float $time): void
+    {
+        $this->setResult(self::TEST_SKIPPED, $test, $message, $time);
+    }
+
+    public function executeAfterIncompleteTest(string $test, string $message, float $time): void
+    {
+        $this->setResult(self::TEST_INCOMPLETE, $test, $message, $time);
+    }
+
+    public function executeAfterLastTest(): void
+    {
+        $client = new Client();
+
+        $res = $client->post('http://localhost:8098/api/1/send_result', [
+            'form_params' => [
+                'api_token' => env('CRAXUS_API_TOKEN', null),
+                'result' => json_encode($this->results)
+            ]
+        ]);
+
+        printf("\n %s", $res->getBody());
+    }
+
+    protected function setResult(string $result, string  $test, string $message, float $time)
+    {
+        $this->results[] = [
+            'number'    => $this->testID,
+            'testName'  => $test,
+            'docBlock'  => $this->docComment,
+            'result'    => $result,
+            'message'   => $message,
+            'time'      => $time
+        ];
+    }
+
+    public function setTestData(string $test)
+    {
+        $this->parseTest($test);
+        $this->getDocComment();
+        $this->getTestId();
+    }
+
+    public function getDocComment()
+    {
+        try {
+            $rc = new ReflectionClass($this->class);
+            $this->docComment = $rc->getMethod($this->method)->getDocComment();
+        } catch (\Exception $exception) {
+            var_dump($exception->getMessage());
+        }
+    }
+
+    public function getTestId()
+    {
+        preg_match_all('#@(.*?)\n#s', $this->docComment, $annotations);
+
+        foreach ($annotations[1] as $annotation)
+            if (strpos($annotation, 'craxus') !== false) {
+                list($a, $this->testID) = explode(' ', $annotation);
+            }
+    }
+
+    public function parseTest($test)
+    {
+        list($this->class, $this->method) = explode('::', $test);
+    }
+}
